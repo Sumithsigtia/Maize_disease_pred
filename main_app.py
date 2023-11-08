b8e8676e822916408aa6dfa6d4dd38949fed9ab7
Sumithsigtia <137398119+Sumithsigtia@users.noreply.github.com>
2023-11-08 16:45:36 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,73 +1,41 @@
-import numpy as np
 import streamlit as st
-import cv2
+import numpy as np
 from keras.models import load_model
-import tensorflow as tf
+from keras.preprocessing import image as keras_image
+from PIL import Image
 
-model=load_model('plant_disease_model (2).h5')
-CLASS_NAMES = ['Corn-Blight','Corn-Common_Rust','Corn-Healthy']
+# Load the trained model
+model_path = "/content/drive/MyDrive/maize_model.h5"  # Replace this with the actual path to your trained model
+model = load_model(model_path)
 
-st.title("Maize(Corn) Plant Leaf Disease Detection")
+# Define class labels
+class_labels = ['Blight', 'Common Rust', 'Healthy']
 
-st.markdown ("Upload an image of the maize (corn) leaf")
+st.title('Maize Leaf Disease Prediction')
 
-plant_image = st.file_uploader("Choose an image...", type="jpg")
-submit= st.button('Predict Disease')
+# Function to make predictions on uploaded image
+def predict_disease(image):
+    img = keras_image.img_to_array(image)
+    img = np.expand_dims(img, axis=0)
+    img = img / 255.0  # Normalizing the image
 
+    prediction = model.predict(img)
+    predicted_class = class_labels[np.argmax(prediction)]
+    confidence = round(np.max(prediction) * 100, 2)
 
+    return predicted_class, confidence
 
-if submit:
-	if plant_image is not None:
-		file_bytes = np.asarray(bytearray(plant_image.read()), dtype=np.uint8)
-		opencv_image = cv2.imdecode(file_bytes, 1)
-		st.image (opencv_image, channels="BGR")
-		st.write (opencv_image.shape)
-		opencv_image = cv2.resize (opencv_image, (256,256))
-		opencv_image.shape=(-1,256,256,3)
-		Y_pred=model.predict (opencv_image)
+# Streamlit app
+st.write("Upload a maize leaf image for disease prediction")
 
-		result=CLASS_NAMES [np.argmax (Y_pred)]
-		y_pred_onehot=tf.keras.utils.to_categorical(Y_pred)
-		result=CLASS_NAMES [np.argmax (y_pred_onehot)]
-		
+file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
 
-		
+if file is not None:
+    img = Image.open(file)
+    st.image(img, caption='Uploaded Image', use_column_width=True)
 
-		st.title (str("This is Maize leaf with "+ result.split('-') [1]))
-
-		if result is 'Corn-Blight':
-			    st.title("Causes for " + result)
-			    st.write("Corn blight in maize can be caused by fungal pathogens such as Helminthosporium maydis or Exserohilum turcicum. These pathogens thrive in warm, humid conditions and can infect corn plants through wounds or natural openings.")
-			    st.write("Factors such as prolonged leaf wetness, high humidity, and warm temperatures favor the development and spread of corn blight.")
-			
-			    st.title("Remedies for " + result)
-			    st.write("Utilize Resistant Varieties - Planting corn varieties that show resistance to the specific blight-causing pathogens can help mitigate the disease's impact.")
-			    st.write("Crop Rotation - Rotate with non-host crops to disrupt the disease cycle and reduce the population of blight-causing pathogens.")
-			    st.write("Fungicides - Application of fungicides, particularly before the onset of favorable conditions for blight, can be an effective management strategy.")
-			    st.write("Sanitation - Clear fields of crop debris post-harvest to reduce overwintering sites for blight pathogens.")
-			    st.write("Adequate Spacing - Avoid overcrowding to ensure good air circulation among plants, reducing humidity and disease spread.")
-			    st.write("Regular Monitoring - Regularly inspect crops for early signs of blight to facilitate prompt action, including removal of infected plants.")
-			
-			    st.write("Consulting local agricultural experts or extension services for region-specific guidance and recommendations can be invaluable in managing and preventing corn blight.")
-
-		
-		if result is 'Corn-Common_Rust':
-			st.title(str("Causes for "+result))
-			st.write(str("Common rust in maize is caused by a fungal pathogen 				      called Puccinia sorghi. This disease occurs when 					      favorable environmental conditions and susceptible 				      host plants come together. Additional reasons 					      maybe Presence of Fungal Spores, Lack of crop 					      rotation")) 
-			st.title(str("Remedies for "+result))
-			st.write(str("Use Fertilizers such as Nitrogen, Phosphorus, 					      Potassium, Organic Fertilizers like Compost, 					      manure "))
-			st.write(str(" also Monitor Regularly-Early detection 						      allows for prompt action"))
-			st.write(str("Nutrient Management-								      Properly nourished plants are more resilient to 					      diseases."))
-			st.write(str("Consult Experts-in case of likelihood of 						      severe infections."))
-
-		if result is 'Corn-Healthy':
-			st.title(str("To Maintain "+result))
-			st.write(str("To maintain the good health of your crop and 					      increase the yield follow Good Agricultural        				      Practices (GAPs) ")) 
-			st.write(str("Fertilization and organic manure: Provide balanced 				      and appropriate fertilization based on soil test 					      results."))
-			st.write(str("Irrigation Management: Water the maize crop 					      consistently and avoid both overwatering and    					      underwatering."))
-			st.write(str("Crop Rotation: Practice crop rotation to break 					     disease cycles and prevent the buildup of pathogens  				     in the soil."))
-			st.write(str("Nutrient Management-								      Properly nourished plants are more resilient to 					      diseases."))
-			
-
-
-				      
+    # Make prediction when the 'Predict' button is clicked
+    if st.button('Predict'):
+        predicted_class, confidence = predict_disease(img)
+        st.write(f'Prediction: {predicted_class}')
+        st.write(f'Confidence: {confidence}%')
